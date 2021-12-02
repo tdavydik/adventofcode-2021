4e5c64fdd96a07fbc8727d6e6f2d7c5cb8aaca8b
Timofei Davydik - HELMES <timofei.davydik@helmes.ee>
2021-12-02 13:58:39 +0300

    Day 1 Part 2

--- src/main/scala/adventofcode/day1/Solution.scala
+++ src/main/scala/adventofcode/day1/Solution.scala
@@ -11,3 +11,14 @@ object Part1 extends Problem {
     }.sum
   }
 }
+
+object Part2 extends Problem {
+  override lazy val day = 1
+
+  override def solution() = {
+    inputInts().sliding(4).foldLeft(0)((sum, w) => w match {
+      case _ if w(3) > w.head => sum + 1
+      case _ => sum
+    })
+  }
+}
